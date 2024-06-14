ffa7680a6d72773a1399513fb2572f431a6338f4
showhack <hellshift0514@gmail.com>
2024-06-14 11:13:24 -0400

    Inicio del frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { login } from "./api/auth"
+
+async function generar() {
+  const a = await login("hellshift", "1234")
+  console.log(a)
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={generar}>
+          click
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/api/auth.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+
+export async function login(username, password) {
+    const res = await axios.post('http://127.0.0.1:8000/api/v1/auth/login/', {
+        username,
+        password,
+    })
+    return res
+}
